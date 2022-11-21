80795f2d9151188ec691a2f2794a07f273d21415
Harpreetd <dhimanharpreet17@yahoo.in>
2022-11-21 23:01:41 +0100

    fetching data using fetch-working

--- src/Hooks/useFetch.ts
+++ src/Hooks/useFetch.ts
@@ -1,26 +1,19 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import axios from "axios";
 
 const useFetch = (url: string) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-
   useEffect(() => {
-    (async function () {
-      try {
-        setLoading(true);
-        const response = await axios.get(url);
-        console.log(response.data);
-        setData(response.data.meals);
-      } catch (err: any) {
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
-    })();
+    setLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false));
   }, [url]);
+
   console.log(data);
   return { data, error, loading };
 };
