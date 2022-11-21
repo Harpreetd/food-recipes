4aa11840ad05b497e6fb418179aec9695f1d7c0d
Harpreetd <dhimanharpreet17@yahoo.in>
2022-11-21 22:55:05 +0100

    fetching data from api using axios-working

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import React from "react";
-
+import Button from "@mui/material/Button";
+import SaveIcon from "@mui/icons-material/Save";
+import TextField from "@mui/material/TextField";
+import useFetch from "./Hooks/useFetch";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h2>Get the Recipes here</h2>
-    </div>
+  const { loading, error } = useFetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s="
   );
+
+  if (error) {
+    console.log(error);
+  }
+
+  return <>{loading && <div>Loading...</div>}</>;
+  // {data && <div>{data.map(item => <div>{item}</div>)}</div>}
+
+  // return (
+  //   <div className="App">
+  //     <h2>Get the Recipes here</h2>
+  //     <TextField variant="outlined" color="secondary" type="date" />
+  //     <Button startIcon={<SaveIcon />} variant="contained" color="secondary">
+  //       Click me
+  //     </Button>
+  //   </div>
+  // );
 }
 
 export default App;
