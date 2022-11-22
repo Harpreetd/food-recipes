c51c3302cf409f74d7fec56fbcdb30bf92f8d229
Harpreetd <dhimanharpreet17@yahoo.in>
2022-11-22 21:49:18 +0100

    getting the right data for one recipe in browser- working

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,109 @@
 import React from "react";
-import Button from "@mui/material/Button";
-import SaveIcon from "@mui/icons-material/Save";
-import TextField from "@mui/material/TextField";
 import { useState, useEffect } from "react";
-// import useFetch from "./Hooks/useFetch";
-import "./App.css";
-import RecipeList from "./Components/RecipeList/RecipeList";
-import { useFetch, MealApiResponse } from "./Hooks/useFetch";
+import { MealApiResponse, useFetch } from "./Hooks/useFetch";
 
-interface Meal {
-  [index: number]: {
-    strArea: string;
-    strCategory: string;
-    strInstructions: string;
-    strMeal: string;
-    strMealThumb: string;
-    strSource: string;
-    strTags: string;
-    strYoutube: string;
-    strIngredient1: string;
-    strIngredient2: string;
-    strIngredient3: string;
-  };
-}
-
-function App() {
-  const [data, dataSet] = useState<Meal>({} as Meal);
-  const testData: MealApiResponse = useFetch(
+const App = () => {
+  const [mealData, setMealData] = useState<MealApiResponse>();
+  const hookData = useFetch(
     "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
   );
-
-  // const { data, loading, error } = useFetch(
-  //   "https://www.themealdb.com/api/json/v1/1/search.php?s="
-  // );
-
-  // if (error) {
-  //   console.log(error);
-  // }
-  // if (!data.loading) console.log(data.data);
+  // console.log(hookData);
   useEffect(() => {
-    if (testData) {
-      dataSet(testData.data as Meal);
+    if (hookData) {
+      setMealData(hookData.apiData);
     }
-  }, [testData]);
-  console.log(data);
+  }, [hookData]);
+  console.log(mealData);
   return (
     <div>
-      {data && (
-        <div>
-          {data[0].strArea} was released in {data[0].strMeal}
-        </div>
-      )}
+      {mealData &&
+        mealData.map((meal, index) => {
+          return (
+            <div key={index}>
+              <h2>Recipe Name: {meal.strMeal}</h2>
+              <h3>From : {meal.strArea}</h3>
+            </div>
+          );
+        })}
     </div>
   );
-  // return (
-  //   <>
-  //     {/* {loading && <div>Loading...</div>} */}
-  //     <RecipeList />
-  //   </>
-  // );
-  // {data && <div>{data.map(item => <div>{item}</div>)}</div>}
-
-  // return (
-  //   <div className="App">
-  //     <h2>Get the Recipes here</h2>
-  //     <TextField variant="outlined" color="secondary" type="date" />
-  //     <Button startIcon={<SaveIcon />} variant="contained" color="secondary">
-  //       Click me
-  //     </Button>
-  //   </div>
-  // );
-}
+};
 
 export default App;
+
+// previous code
+// import React from "react";
+// import Button from "@mui/material/Button";
+// import SaveIcon from "@mui/icons-material/Save";
+// import TextField from "@mui/material/TextField";
+// import { useState, useEffect } from "react";
+// // import useFetch from "./Hooks/useFetch";
+// import "./App.css";
+// import RecipeList from "./Components/RecipeList/RecipeList";
+// import { useFetch, MealApiResponse } from "./Hooks/useFetch";
+
+// interface Meal {
+//   [index: number]: {
+//     strArea: string;
+//     strCategory: string;
+//     strInstructions: string;
+//     strMeal: string;
+//     strMealThumb: string;
+//     strSource: string;
+//     strTags: string;
+//     strYoutube: string;
+//     strIngredient1: string;
+//     strIngredient2: string;
+//     strIngredient3: string;
+//   };
+// }
+
+// function App() {
+//   const [data, dataSet] = useState<Meal>({} as Meal);
+//   const testData: MealApiResponse = useFetch(
+//     "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
+//   );
+
+//   // const { data, loading, error } = useFetch(
+//   //   "https://www.themealdb.com/api/json/v1/1/search.php?s="
+//   // );
+
+//   // if (error) {
+//   //   console.log(error);
+//   // }
+//   // if (!data.loading) console.log(data.data);
+//   useEffect(() => {
+//     if (testData) {
+//       dataSet(testData.data as Meal);
+//     }
+//   }, [testData]);
+//   console.log(data);
+//   return (
+//     <div>
+//       {data && (
+//         <div>
+//           {data[0].strArea} was released in {data[0].strMeal}
+//         </div>
+//       )}
+//     </div>
+//   );
+//   // return (
+//   //   <>
+//   //     {/* {loading && <div>Loading...</div>} */}
+//   //     <RecipeList />
+//   //   </>
+//   // );
+//   // {data && <div>{data.map(item => <div>{item}</div>)}</div>}
+
+//   // return (
+//   //   <div className="App">
+//   //     <h2>Get the Recipes here</h2>
+//   //     <TextField variant="outlined" color="secondary" type="date" />
+//   //     <Button startIcon={<SaveIcon />} variant="contained" color="secondary">
+//   //       Click me
+//   //     </Button>
+//   //   </div>
+//   // );
+// }
+
+// export default App;
--- src/Hooks/useFetch.ts
+++ src/Hooks/useFetch.ts
@@ -2,15 +2,28 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 export type MealApiResponse = {
-  data: any;
-  error: any;
-  loading: boolean;
+  map(arg0: (meal: any, index: any) => JSX.Element): React.ReactNode;
+  strArea: string;
+  strCategory: string;
+  strInstructions: string;
+  strMeal: string;
+  strMealThumb: string;
+  strSource: string;
+  strTags: string;
+  strYoutube: string;
+  strIngredient1: string;
+  strIngredient2: string;
+  strIngredient3: string;
 };
 
-export const useFetch = (url: string): MealApiResponse => {
-  const [data, setData] = useState<any>();
+export const useFetch = (url: string) => {
+  const [apiData, setApiData] = useState<MealApiResponse>();
   const [error, setError] = useState<any>();
   const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    getApiData();
+  }, [url]);
 
   const getApiData = async () => {
     setLoading(true);
@@ -18,16 +31,46 @@ export const useFetch = (url: string): MealApiResponse => {
       const apiResponse = await fetch(url);
       const json = await apiResponse.json();
       console.log(json.meals);
-      setData(json.meals);
+      setApiData(json.meals);
     } catch (error) {
       setError(error);
     }
     setLoading(false);
   };
 
-  useEffect(() => {
-    getApiData();
-  }, [url]);
-  return { data, error, loading };
+  return { apiData: apiData };
 };
+
+// import React from "react";
+// import { useState, useEffect } from "react";
+
+// export type MealApiResponse = {
+//   data: any;
+//   error: any;
+//   loading: boolean;
+// };
+
+// export const useFetch = (url: string): MealApiResponse => {
+//   const [data, setData] = useState<any>();
+//   const [error, setError] = useState<any>();
+//   const [loading, setLoading] = useState<boolean>(false);
+
+//   const getApiData = async () => {
+//     setLoading(true);
+//     try {
+//       const apiResponse = await fetch(url);
+//       const json = await apiResponse.json();
+//       console.log(json.meals);
+//       setData(json.meals);
+//     } catch (error) {
+//       setError(error);
+//     }
+//     setLoading(false);
+//   };
+
+//   useEffect(() => {
+//     getApiData();
+//   }, [url]);
+//   return { data, error, loading };
+// };
 // export default useFetch;
