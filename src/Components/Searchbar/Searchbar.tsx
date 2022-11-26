5cc93d8e81217f5a766dc48c0f8e918f1b4cba45
Harpreetd <dhimanharpreet17@yahoo.in>
2022-11-26 22:59:40 +0100

    dont remmeber

--- src/Components/Searchbar/Searchbar.tsx
+++ src/Components/Searchbar/Searchbar.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useGlobalContext } from "../../Context/AppContext";
 const Searchbar = () => {
   const { setSearchTerm } = useGlobalContext();
   const searchValue = React.useRef<HTMLInputElement>(null);
   // console.log(setSearchTerm);
+  useEffect(() => {
+    searchValue.current?.focus();
+  }, []);
   const searchMeal = () => {
     if (setSearchTerm) setSearchTerm(searchValue.current?.value);
   };
+  const handleSubmit = (e: Event) => {
+    e.preventDefault();
+  };
   return (
     <section>
-      <form>
+      <form onSubmit={() => handleSubmit}>
         <div>
           <label htmlFor="mealName">Search recipe name</label>
           <input
--- src/Pages/SingleMeal.tsx
+++ src/Pages/SingleMeal.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from "react";
-
+import { useParams, Link } from "react-router-dom";
+const url = "www.themealdb.com/api/json/v1/1/lookup.php?i";
 const SingleMeal: FC = () => {
+  const { id } = useParams<string>();
+  console.log(id);
   return <div>SingleMeal</div>;
 };
 
